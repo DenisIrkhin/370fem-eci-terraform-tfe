f69c1a1a184322b5cb04e209cb92ea9693f86944
Denis Irkhin <devdenis555@gmail.com>
2024-07-23 11:52:12 -0400

    connect github to TFcloud

--- locals.tf
+++ locals.tf
@@ -8,8 +8,9 @@ locals {
   workspace = {
     "fem-eci-tfe" = {
       description = "This is workspace description"
-      execution_mode = "local"
+      execution_mode = "remote"
       project_id = module.project["fem-eci-project"].id
+      vcs_repo_identifier = "denisirkhin/370fem-eci-terraform-tfe"
     }
 
   }
--- variables.tf
+++ variables.tf
@@ -2,3 +2,13 @@ variable "organization_name" {
   default = "555"
   type = string
 }
+
+variable "tfe_github_app_installation_id" {
+  default = 53091004
+  type = number
+}
+
+variable "github_organization_name" {
+  default = "DenisIrkhin"
+  type = string
+}
