20626b6cf1d7fef7f880142f79fb42ca3c75a115
Denis Irkhin <devdenis555@gmail.com>
2024-07-23 16:07:25 -0400

    feat: add Github workspace

--- locals.tf
+++ locals.tf
@@ -7,10 +7,16 @@ locals {
   
   workspace = {
     "fem-eci-tfe" = {
-      description = "This is workspace description"
+      description = "This is first workspace for managing TFE"
       execution_mode = "remote"
       project_id = module.project["fem-eci-project"].id
       vcs_repo_identifier = "${var.github_organization_name}/370fem-eci-terraform-tfe"
+    },
+    "fem-eci-github" = {
+      description = "This is second workspace for managing GitHub"
+      execution_mode = "local"
+      project_id = module.project["fem-eci-project"].id
+      vcs_repo_identifier = "${var.github_organization_name}/371fem-eci-terraform-github"
     }
 
   }
