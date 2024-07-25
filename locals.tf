3bb0131528b301e3b3ee1d5af604c3b2ecba1194
Denis Irkhin <devdenis555@gmail.com>
2024-07-24 20:04:06 -0400

    feat: add workspaces for network and cluster

--- locals.tf
+++ locals.tf
@@ -19,5 +19,61 @@ locals {
       vcs_repo_identifier = "${var.github_organization_name}/371fem-eci-terraform-github"
     }
 
+    "fem-eci-network" = {
+      description = "This is third workspace for managing AWS networks"
+      execution_mode = "remote"
+      project_id = module.project["fem-eci-project"].id
+      vcs_repo_identifier = "${var.github_organization_name}/372fem-eci-terraform-network"
+
+      variables = [
+        {
+          category = "terraform"
+          hcl      = true
+          key      = "azs"
+          value    = jsonencode(["us-west-2a", "us-west-2b"])
+        },
+        {
+          category = "terraform"
+          key      = "cidr"
+          value    = "10.0.0.0/16"
+        },
+        {
+          category = "terraform"
+          key      = "name"
+          value    = "fem-eci"
+        },
+      ]  
+    }
+
+    "fem-eci-clusters-prod" = {
+      description = "This is forth workspace for managing AWS clusters"
+      execution_mode = "remote"
+      project_id = module.project["fem-eci-project"].id
+      vcs_repo_identifier = "${var.github_organization_name}/372fem-eci-terraform-cluster"
+
+            variables = [
+        {
+          category = "terraform"
+          key      = "domain"
+          value    = "idenis.click"
+        },
+        {
+          category = "terraform"
+          key      = "environment"
+          value    = "prod"
+        },
+        {
+          category = "terraform"
+          key      = "name"
+          value    = "fem-eci-denisirkhin"
+        },
+        {
+          category = "terraform"
+          key      = "vpc_name"
+          value    = "fem-eci"
+        },
+      ]
+    }
+
   }
 }
